7b95446aaa6904a2320b37643970a0c4de435308
seolyooo <syj011128@naver.com>
2025-07-15 15:29:29 +0900

    수정

--- ch05/src/sub6/Eagle.java
+++ ch05/src/sub6/Eagle.java
@@ -4,11 +4,11 @@ public class Eagle extends Animal {
 	
 	@Override
 	public void move() {
+		
 		System.out.println("Eagle fly...");
 	}
 	@Override
 	public void hunt() {
 		System.out.println("eagle hunt...");
 	}
-	
 }
